79777912eab350bdd403530bcfd1b48382a72e5d
Hasyama <xasyama@gmail.com>
2024-01-08 15:50:06 +0300

    First Vue SignUp form prototype

---
+++ Vue-training/vue-login-form/vue-login-form-1/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <SignUpForm /> <!-- using the component -->
+</template>
+
+<script>
+import SignUpForm from './components/SignUpForm.vue'
+
+export default {
+  name: 'App',
+  components: {  SignUpForm  } // activate to use the component in html
+}
+</script>
+
+<style>
+#app {
+  font-family: Arial, Helvetica, sans-serif;
+  --webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+body {
+  margin: 0;
+  background: #eee;
+}
+</style>
